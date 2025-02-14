73882ed44d088f7d0158b4c10a941fc3f56b61ad
mael.larue <mael.larue@mmibordeaux.com>
2025-02-14 20:46:33 +0100

    responsive projets

--- src/stylesheet/script.js
+++ src/stylesheet/script.js
@@ -26,23 +26,31 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("scroll", () => {
   const navigation = document.querySelector(".naviguation");
   const contact = document.querySelector(".contact");
+  const jobList = document.querySelector(".job-list");
 
   let isNavigationVisible = false;
   let isContactVisible = false;
+  let isJobListCentered = false;
 
   if (navigation) {
     const rectNav = navigation.getBoundingClientRect();
     isNavigationVisible =
-      rectNav.top <= window.innerHeight / 2 && rectNav.bottom >= 30;
+      rectNav.top < window.innerHeight / 2 && rectNav.bottom > window.innerHeight / 3;
   }
 
   if (contact) {
     const rectContact = contact.getBoundingClientRect();
     isContactVisible =
-      rectContact.top <= window.innerHeight / 2 && rectContact.bottom >= 30;
+      rectContact.top < window.innerHeight / 2 && rectContact.bottom > window.innerHeight / 3;
   }
 
-  if (isNavigationVisible || isContactVisible) {
+  if (jobList) {
+    const rectJob = jobList.getBoundingClientRect();
+    isJobListCentered =
+      rectJob.top <= window.innerHeight / 2 && rectJob.bottom >= window.innerHeight / 2;
+  }
+
+  if ((isNavigationVisible || isContactVisible) && !isJobListCentered) {
     document.body.style.backgroundColor = "black";
   } else {
     document.body.style.backgroundColor = "white";
